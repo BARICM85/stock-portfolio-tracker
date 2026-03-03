9e81639656316fbf6854b4f8feaffe867311689f
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-03 20:08:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ async function showPortfolio() {
 
         const color = gain >= 0 ? "green" : "red";
       
-for (let index = 0; index < portfolio.length; index++) {
+for (let index = 0; index < portfolio.length; index++) }
 
     let stock = portfolio[index];
 
@@ -494,6 +494,7 @@ window.showLogin = showLogin;
 window.register = register;
 window.login = login;
 window.deleteStock = deleteStock;
+
 
 
 
