07541e2ee020c6900d0f3a7b119d91bfef292025
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-03 18:48:29 +0530

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,17 +1,8 @@
-function loadPortfolio() {
+export function loadPortfolio() {
     const data = localStorage.getItem("portfolio");
     return data ? JSON.parse(data) : [];
 }
 
-function savePortfolio(portfolio) {
+export function savePortfolio(portfolio) {
     localStorage.setItem("portfolio", JSON.stringify(portfolio));
-
-    // ALSO SAVE TO FIREBASE IF LOGGED IN
-    const user = auth.currentUser;
-    if (!user) return;
-
-    firebaseHelpers.setDoc(
-        firebaseHelpers.doc(db, "portfolios", user.uid),
-        { stocks: portfolio }
-    );
 }
