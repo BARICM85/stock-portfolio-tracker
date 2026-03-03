a0b4ac7458e31e3d2f31e1732843c018ecb49842
BARICM85 <64754982+BARICM85@users.noreply.github.com>
2026-03-03 18:40:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 import { loadPortfolio, savePortfolio } from "./storage.js";
-export function loadPortfolio() {
-    return JSON.parse(localStorage.getItem("portfolio")) || [];
-}
 
-export function savePortfolio(portfolio) {
-    localStorage.setItem("portfolio", JSON.stringify(portfolio));
-}
 // 🔥 Firebase Imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
@@ -428,6 +422,7 @@ window.downloadExcel = downloadExcel;
 window.showLogin = showLogin;
 window.register = register;
 window.login = login;
+
 
 
 
